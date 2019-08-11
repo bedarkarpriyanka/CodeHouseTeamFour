762f22a781c6ba86bf109b0369b8fff246d5c285
Priyanka Bedarkar <bedarkar.priyanka95@gmail.com>
2019-08-11 08:43:38 -0700

    login and register

---
+++ app.py
@@ -0,0 +1,216 @@
+import os
+from flask import Flask, url_for, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import form, fields, validators
+import flask_admin as admin
+import flask_login as login
+from flask_admin.contrib import sqla
+from flask_admin import helpers, expose
+from werkzeug.security import generate_password_hash, check_password_hash
+import string
+import random
+import datetime
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456790'
+app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    login = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(64))
+    status = db.Column(db.String(30))
+    org_name = db.Column(db.String(100))
+    interest = db.Column(db.String(100))
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __unicode__(self):
+        return self.username
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    q_created_at = db.Column(db.DateTime, datetime.datetime.utcnow)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __unicode__(self):
+        return self.username
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    login = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(64))
+    status = db.Column(db.String(30))
+    org_name = db.Column(db.String(100))
+    interest = db.Column(db.String(100))
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __unicode__(self):
+        return self.username
+
+
+class LoginForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+    def validate_login(self, field):
+        user = self.get_user()
+        if user is None:
+            raise validators.ValidationError('Invalid user')
+        if not check_password_hash(user.password, self.password.data):
+            raise validators.ValidationError('Invalid password')
+    def get_user(self):
+        return db.session.query(User).filter_by(login=self.login.data).first()
+
+
+class RegistrationForm(form.Form):
+    first_name = fields.StringField(validators=[validators.required()])
+    last_name = fields.StringField(validators=[validators.required()])
+    login = fields.StringField(validators=[validators.required()])
+    email = fields.StringField(validators=[validators.required(), validators.Email("Invalid Email")])
+    status = fields.StringField(validators=[validators.required(),
+                validators.AnyOf(message="Choose Status among 'High School', 'College', 'Industry'",
+                values=['High School', 'College', 'Industry'])])
+    org_name = fields.StringField(validators=[validators.required()])
+    interest = fields.StringField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+    def validate_login(self, field):
+        if db.session.query(User).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
+
+
+
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+
+class MyModelView(sqla.ModelView):
+    def is_accessible(self):
+        return login.current_user.is_authenticated
+
+
+class MyAdminIndexView(admin.AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated:
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            login.login_user(user)
+        if login.current_user.is_authenticated:
+            return redirect(url_for('.index'))
+        link = '<p>Don\'t have an account? <a href="' + url_for('.register_view') + '">Click here to register.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/register/', methods=('GET', 'POST'))
+    def register_view(self):
+        form = RegistrationForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = User()
+            form.populate_obj(user)
+            user.password = generate_password_hash(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            login.login_user(user)
+            return redirect(url_for('.index'))
+        link = '<p>Already have an account? <a href="' + url_for('.login_view') + '">Click here to log in.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        login.logout_user()
+        return redirect(url_for('.index'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+init_login()
+admin = admin.Admin(app, 'Team Four', index_view=MyAdminIndexView(), base_template='my_master.html')
+admin.add_view(MyModelView(User, db.session))
+
+
+def build_sample_db():
+    db.drop_all()
+    db.create_all()
+    test_user = User(login="test", password=generate_password_hash("test"))
+    test_question = Question()
+    db.session.add(test_user)
+    db.session.add(test_question)
+    db.session.commit()
+    return
+
+if __name__ == '__main__':
+    app_dir = os.path.realpath(os.path.dirname(__file__))
+    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
+    if not os.path.exists(database_path):
+        build_sample_db()
+    app.run(debug=True)
